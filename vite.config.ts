04e349721e024c4e26bfeefb4cc43c6419b1e927
umarcrystal <crystaltubesumar@gmail.com>
2025-05-02 01:04:39 +0530

    github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/crystal-tubes-2025/', // Base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
