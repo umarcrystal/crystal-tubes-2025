851ad1e3784f1487d98fe9b6689fb0a2a48ce609
umarcrystal <crystaltubesumar@gmail.com>
2025-05-02 16:15:33 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/crystal-tubes-2025/' : '/',
+  base: mode === 'production' ? '/' : '/',
   server: {
     host: "::",
     port: 8080,
