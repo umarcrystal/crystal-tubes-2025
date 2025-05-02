5d256c28eb9165458ce0e1b9cee8431789401079
umarcrystal <crystaltubesumar@gmail.com>
2025-05-02 16:24:42 +0530

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -90,20 +90,20 @@ const Footer: React.FC = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
                 <span className="text-gray-400">
-                  123 Industrial Area, Andheri East, Mumbai 400072, Maharashtra, India
+                  Survey no. 48 Village Bhandarli, NH48, Thane, Maharashtra 400612
                 </span>
               </li>
               <li className="flex">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-steel mr-2 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <span className="text-gray-400">+91 22 1234 5678</span>
+                <span className="text-gray-400">+91 93246 29085</span>
               </li>
               <li className="flex">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-steel mr-2 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <span className="text-gray-400">info@mumbaisteel.com</span>
+                <span className="text-gray-400">crystaltubes.msk95@gmail.com</span>
               </li>
             </ul>
           </div>
